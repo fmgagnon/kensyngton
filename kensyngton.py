afc6826b86036b4dded56406ad0ab9d34c71fb1a
fmgagnon <franfois@gmail.com>
2011-07-07 09:27:28 -0700

    réécrit la phase un...

--- kensyngton.py
+++ kensyngton.py
@@ -1,13 +1,15 @@
-
 # l'audace a un nom : une liste et un dictionnaire pour, respectivement, le journal et la planche de jeu.
 journal = []
 # proposition : chaque entrée a un index numérique, un flag pour indiquer si le mouvement est forcé et les trois informations communiquées par mvt.
 
 
+
+import random
+
 # d’accord, ce n’est pas joli, mais ça permet d’interroger facilement l’état de chacun des points du jeu.
 #	en faisant, par exemple, planche[‘A1’].
 # Les valeurs sont ‘R’, ‘B’ et ‘’ (vide)
-planche = {'A1': '', 'A2': '', 'A3': '', 'B1': '', 'B2': '', 'B3': '', 'C1': '', 'C2': '', 'C3': '', 'D1': '', 'D2': '', 'D3': '', 'E1': '', 'E2': '', 'E3': '', 'F1': '', 'F2': '', 'F3': '', 'G1': '', 'G2': '', 'G3': '', 'H1': '', 'H2': '', 'H3': '', 'I1': '', 'I2': '', 'I3': '', 'J1': '', 'J2': '', 'J3': '', 'K1': '', 'K2': '', 'K3': '', 'L1': '', 'L2': '', 'L3': '', 'M1': '', 'M2': '', 'M3': '', 'N1': '', 'N2': '', 'N3': '', 'O1': '', 'O2': '', 'O3': '', 'P1': '', 'P2': '', 'P3': '', 'Q1': '', 'Q2': '', 'Q3': '', 'R1': '', 'R2': '', 'R3': '', 'S1': '', 'S2': '', 'S3': '', 'T1': '', 'T2': '', 'T3': '', 'U1': '', 'U2': '', 'U3': '', 'V1': '', 'V2': '', 'V3': '', 'W1': '', 'W2': '', 'W3': '', 'X1': '', 'X2': '', 'X3': ''}
+planche = {'A1': '', 'A2': '', 'A3': '', 'B1': '', 'B2': '', 'B3': '', 'C1': '', 'C2': '', 'C3': '', 'D1': '', 'D2': '', 'D3': '', 'E1': '', 'E2': '', 'E3': '', 'F1': '', 'F2': '', 'F3': '', 'G1': '', 'G2': '', 'G3': '', 'H1': '', 'H2': '', 'H3': '', 'I1': '', 'I2': '', 'I3': '', 'J1': '', 'J2': '', 'J3': '', 'K1': '', 'K2': '', 'K3': '', 'L1': '', 'L2': '', 'L3': '', 'M1': '', 'M2': '', 'M3': '', 'N1': '', 'N2': '', 'N3': '', 'O1': '', 'O2': '', 'O3': '', 'P1': '', 'P2': '', 'P3': '', 'Q1': '', 'Q2': '', 'Q3': '', 'R1': '', 'R2': '', 'R3': '', 'S1': '', 'S2': '', 'S3': '', 'T1': '', 'T2': '', 'T3': '', 'U1': '', 'U2': '', 'U3': '', 'V1': '', 'V2': '', 'V3': '', 'W1': '', 'W2': '', 'W3': '', 'X1': '', 'X2': '', 'X3': '', 'NIL': ''}
 
 # pour que les fonctions puissent communiquer entre elles, je propose une liste appelée mvt qui comprend les infos et est ensuite encastrée dans le journal de jeu.
 # mvt = [coul, orig, dest]
@@ -51,9 +53,11 @@ def question():
 		legal(mvt)
 	return mvt
 
+
 def carre():
 
 def triangle():
+
 
 def verif():
 	# vérifie si un carré ou un triangle a été complété
@@ -68,44 +72,35 @@ def verif():
                 si oui : fin(coul,hex)
                 
 
-
-
-
-def mouvement():
-	journal.append(mvt)
-	planche[mvt[dest]] = mvt[coul]
-
-def mouvementforce():
-	mvt = '!', mvt
-	journal.append(mvt)
-
-
-
-def tour():
-    i = 0
-    while i = 0:
-        mvt = question()
-        mouvement(mvt)
-        verif()
-
-def legal_phaseun(check):
-    
 def phaseun_rouge():
     # Je sépare les deux pour que la programmation de l'AI soit plus claire.
-    
     # Rouge = AI
+
+    point = 'NIL'       # un petit bidouillis pour pouvoir initier la boucle.
+
+    while planche['point'] != '':
+        # choisit un point (pour l'instant au hasard)
+        point =    # ça prendrait un petit code pour choisir un point...
+       
+    planche['point'] = 'R'       # on indique le pion sur la plane
+    journal.append("R " + point)  # le mouvement est indiqué dans le journal
 
 def phaseun_bleu():
     # Ici, le code pour le joueur humain.
-    
-	
-    while planche[point] != '':
-        # interroge sur le pion à placer
-        point = legal_phaseun(raw_input("Sur quel point mettre un pion? "))
+    tour = 0
 
-		# indique le pion sur 'planche'
-			
-		# indique le mouvement dans le 'journal'
+    while tour == 0:
+        # interroge sur le pion à placer
+        point = raw_input("Sur quel point mettre un pion? ")
+        if point not in planche:
+            print "Ce point n'existe pas."
+        if planche['point'] != '':
+            print "Ce point est occupé."
+        else:
+            planche['point'] = 'B'       # on indique le pion sur la plane
+            journal.append("B " + point)  # le mouvement est indiqué dans le journal
+            tour = 1                     # on brise la boucle pour finir le tour
+
 
 def phaseun(quicommence):
     # Une phase facile à programmer : Les joueurs placent, tour à tour, des pions.
@@ -120,13 +115,14 @@ def phaseun(quicommence):
         while i < 14;
             phaseun_rouge()
             phaseun_bleu()
-            i += 1    
+            i += 1
+
 def phasedeux():
     # La phase du jeu la plus importante...    
 
 # Le jeu en tant que tel devrait appeler deux boucles principales : Le premier tour, où les joueurs emplissent la planche, et le second, où il s'agit de déplacements.
 
 # La variable quicommence sert à décider... bon... devine... comme le jeu est symétrique, le même joueur commence les deux phases.
-quicommence = randint(0,1)
+quicommence = randint(0,2)
 phaseun(quicommence)
 phasedeux(quicommence)
